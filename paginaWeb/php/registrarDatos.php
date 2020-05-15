2ab3538a2918d5a342e25669b9181d9004fcd98f
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-05-15 11:22:49 -0500

    Desarrollado entorno de envio de formulario

--- paginaWeb/php/registrarDatos.php
+++ paginaWeb/php/registrarDatos.php
@@ -1,5 +1,21 @@
 <?php
 require("conexion.php");
 $correo = $_POST['correo'];
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+$adminCode = $_POST['adminCode'];
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$edad = $_POST['edad'];
+$telefono = $_POST['telefono'];
+
 echo ($correo);
+echo ($usuario);
+echo ($contraseña);
+echo ($adminCode);
+echo ($nombre);
+echo ($apellidos);
+echo ($edad);
+echo ($telefono);
+
 ?>
