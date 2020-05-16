a33504b184e3d5e1960112fb94f027b3804e3d96
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-05-16 09:13:20 -0500

    Creada pantalla de error del login

--- paginaWeb/php/login.php
+++ paginaWeb/php/login.php
@@ -10,39 +10,42 @@ $resultado = mysqli_query($conexion, $consulta);
 $tipoUsuario = "";
 $nombre = "";
 $usuarioEncontrado = false;
+$claveCorrecta = false;
 while ($columna = mysqli_fetch_array( $resultado ))
 {
 	if ($columna['correo']==$correo) {
+		$usuarioEncontrado = true;
 		if ($columna['contraseña']==$contraseña) {
 			$tipoUsuario = $columna['tipoDeUsuario'];
 			$nombre = $columna['nombres'];
-			$usuarioEncontrado = true;
+			$claveCorrecta = true;
 			}
         }
     if ($columna['usuario']==$usuario) {
+		$usuarioEncontrado = true;
 		if ($columna['contraseña']==$contraseña) {
 			$tipoUsuario = $columna['tipoDeUsuario'];
 			$nombre = $columna['nombres'];
-			$usuarioEncontrado = true;
+			$claveCorrecta = true;		
 			}
         }
         
 }
 
-if ($usuarioEncontrado) {
+if ($usuarioEncontrado && $claveCorrecta) {
     session_start();
     $_SESSION['nombre'] = $nombre;
     $_SESSION['usuario'] = $usuario;
     $_SESSION['tipoUsuario'] = $tipoUsuario;
 	switch ($tipoUsuario) {
 		case 1:
-			header("Location:../home/homeAdmin.php");
+			header("Location:../home/homeAdmin.html");
 			break;
 		case 2:
-			header("Location:../home/homeUser.php");
+			header("Location:../home/homeUser.html");
 			break;
 	}
-} else {
+} else{
 	header("Location:../errorLogin.html");
 }
 ?>
