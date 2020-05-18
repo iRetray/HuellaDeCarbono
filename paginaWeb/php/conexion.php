0f502afac446642acc17afb13911c5155138f96b
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-05-18 13:06:11 -0500

    Organizacion nueva

--- paginaWeb/php/conexion.php
+++ paginaWeb/php/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servidor = "localhost";
-$usuarioBD = "id13717416_root";
-$contraseña = "<<z~WR9Ju(2hm@wg";
-$baseDeDatos = "id13717416_footprint";
+$usuarioBD = "root";
+$contraseña = "";
+$baseDeDatos = "footprint";
 $conexion = mysqli_connect($servidor, $usuarioBD, $contraseña, $baseDeDatos) or die ("La conexion al servidor no fue establecida");
 $db = mysqli_select_db($conexion,$baseDeDatos) or die ("No se logro conectar a la base de datos");
 ?>
