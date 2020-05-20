c01665b5f53802fff4b9ae16cc483e7e22f7033b
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-05-20 10:43:18 -0500

    Creado formulario de huella de carbono

--- paginaWeb/homePages/calculoHuella.php
+++ paginaWeb/homePages/calculoHuella.php
@@ -40,7 +40,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>Home</title>
+    <title>Calculadora</title>
 </head>
 <body class="gris">
 
@@ -57,58 +57,84 @@
 		</form>
 	</nav>
 <div id="appVue" class="padreHome">
-
-
-<div class="jumbotron">
-    <h1>{{ nombre }} <span class="badge badge-secondary"><i class="fas fa-users"></i> Usuario nuevo</span></h1>
-    <div v-if="hayOrganizacion">
-        <h4><span class="badge badge-success"><i class="fas fa-university"></i> {{ organizacion }}</span></h4>
-    </div>
-    <div v-else>
-        <form action="../php/ingresarEntidad.php" method="POST">
-            {{ organizacion }}
-        No perteneces a ninguna organización.<br>
-        <input type="text" placeholder="Código de entidad" required="true" name="codigo">
-        <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-sign-in-alt"></i> Unirme ahora</button>
-        </form>
-    </div>
-    <hr class="my-4">
-    <small class="text-muted">Ver mis datos personales</small><br>
-    <label class="switch">
-        <input type="checkbox"
-        v-model="conCorreo">
-        <span class="slider round"></span>
-    </label>
-    <div v-if="isChecked">
-        <div class="alert alert-secondary" role="alert">
-            <h4>{{ nombre }} {{ apellido }}</h4>
-            <p v-if="hayOrganizacion"><i class="fas fa-university"></i> Organización: {{ organizacion }}</p>
-            <p v-else><i class="fas fa-university"></i> Organización: Ninguna</p>
-            
-            <small>{{ tipoUsuario }}</small>
-            <p><strong>Nombre de usuario: </strong> {{ usuario }}<br>
-            <strong>Edad: </strong> {{ edad }}<br>
-            <strong>Correo electrónico: </strong> {{ correo }}<br>
-            <strong>Teléfono: </strong> {{ telefono }}</p>
-        </div> 
-    </div>
-    
-    <hr class="my-4">
-    <p class="lead">Bienvenido! Esta es tu plataforma de inicio en <strong>iFootprint</strong></p>
-    <div class="alert alert-primary" role="alert">
-        <center>
-    <i class="fas fa-exclamation-triangle"></i> Ahora podrás realizar <strong>calculos de tu huella de carbono</strong>, y estos quedarán guardados en tu <strong>historial</strong>.
-    </center>    
-</div>
-    <div class="alert alert-success" role="alert">
-        <center>
-    <i class="fas fa-question-circle"></i> ¡Vaya! <strong>Detectamos que aún no has realizado tu primer examen<br><br>
-    <button type="button" class="btn btn-success"><i class="fas fa-feather-alt"></i> Realizar mi primer examen</button>
+    <div class="calculadora ">
+<div class="calculadora jumbotron">
+    <center>
+        <p class="display-4">Calculadora de Huella de carbono</p>
+        <div class="alert alert-success" role="alert">
+        La huella de carbono es un indicador ambiental que se expresa en las <strong>toneladas de CO2</strong>, buscando expresar la cantidad de gases efecto invernadero que emiten las actividades de cada ser humano. 
+        </div>
+        <hr class="my-4">
+        <h3>El impacto ambiental que generas es de: <span class="badge badge-warning"> {{ huellaTotal }} </span> <small>Toneladas de CO2</small> </h3>
+        <hr class="my-4">   
     </center>
+    <center>
+    <b-form @submit="checkForm" action="subirCalculo.php" method="post" novalidate="true"
+    v-on:submit.prevent="checkForm" name="formRegistro">
+    <!-- Pregunta 1 --> 
+    <p class="lead">¿Usas autobuses para movilizarte?</p>
+        <label class="switch">  
+            <input type="checkbox"
+            v-model="usarBus">
+            <span class="slider round"></span>
+        </label>
+    <div class="form-group" v-if="usarBus">
+        <div class="alert alert-secondary" role="alert">        
+        <small>
+            <p>¿Cuántos dias a la semana? <strong>{{ diasBus }}</strong> </p>              
+        </small>
+            <input 
+            type="range" 
+            id="diasBus" 
+            v-model="diasBus" 
+            name="diasBus"
+            max=7>
+            </input>
+        <div v-if="diasBus!=0">
+        <small>
+            <p>¿Cuántas horas al dia? <strong>{{ horasBus }}</strong> </p>              
+        </small>
+            <input 
+            type="range" 
+            id="horasBus" 
+            v-model="horasBus" 
+            name="horasBus"
+            max=24>
+            </input>
+        </div>
     </div>
+    </div>
+
+    <!-- Pregunta 2 --> 
+    <p class="lead">¿Consumes carne de res?</p>
+        <label class="switch">  
+            <input type="checkbox"
+            v-model="comerCarne">
+            <span class="slider round"></span>
+        </label>
+    <div class="form-group" v-if="comerCarne">
+        <div class="alert alert-secondary" role="alert">        
+        <small>
+            <p>¿Cuántos dias a la semana? <strong>{{ diasCarne }}</strong> </p>              
+        </small>
+            <input 
+            type="range" 
+            id="diasCarne" 
+            v-model="diasCarne" 
+            name="diasCarne"
+            max=7>
+            </input>        
+    </div>
+    </div>
+        
+    </div>
+    </div>
+    </b-form>
+    </center>
+
+
 </div>
-
-
+</div>
 </div>
 
 
@@ -117,41 +143,23 @@
             const app = new Vue({
             el:'#appVue',
             data:{
-                conCorreo: false                
+                usarBus: false,
+                diasBus: 0,
+                horasBus: 0,
+                comerCarne: false,
+                diasCarne: 0
             },
             computed: {
-                noTieneCalculos() {
-
-                },
-                isChecked() {
-                    return this.conCorreo;
-                },
-                nombre() {
-                    return "<?php echo $nombre; ?>";
-                },
-                apellido() {
-                    return "<?php echo $apellido; ?>";
-                },
-                usuario() {
-                    return "<?php echo $usuario; ?>";
-                },
-                tipoUsuario() {
-                    return "<?php echo $tipo; ?>";
-                },
-                edad() {
-                    return "<?php echo $edad; ?>";
-                },
-                correo() {
-                    return "<?php echo $correo; ?>";
-                },
-                telefono() {
-                    return "<?php echo $telefono; ?>";
-                },
-                organizacion() {
-                    return "<?php echo $nombreEntidad; ?>";
-                },
-                hayOrganizacion() {
-                    return "<?php echo $nombreEntidad; ?>".lenght;
+                huellaTotal() {
+                    huella = 0;
+                    if (this.usarBus) {
+                        huella = huella + 0.2*this.diasBus + 0.2*this.horasBus;
+                    }
+                    if (this.comerCarne) {
+                        huella = huella + 2.9*this.diasCarne;
+                    }
+                    huella = huella.toFixed(2);
+                    return huella;
                 }
             },
             methods:{
