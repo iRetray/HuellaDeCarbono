98fb8d743f579ff4e4080cda6b4c7dd9f5349c7f
Julian Camilo <47187585+iRetray@users.noreply.github.com>
2020-06-12 23:33:00 -0500

    Configurado chartJS

--- paginaWeb/homePages/homeUser.php
+++ paginaWeb/homePages/homeUser.php
@@ -64,81 +64,83 @@
 			<button class="btn btn-danger" type="submit"><i class="fas fa-door-open"></i> Cerrar sesión</button>
 			</div>
 		</form>
-	</nav>
-<div id="appVue" class="padreHome">
+    </nav>
 
-<div class="row">
-    <div class="col-4">
-    <div class="calculadora jumbotron">
-    <h1>{{ nombre }} <span class="badge badge-secondary"><i class="fas fa-users"></i> Usuario nuevo</span></h1>
-    <h3 v-show="haHechoExamen">Promedio de huella de carbono: <span class="badge badge-warning"><i class="fas fa-biohazard"></i> {{ promedio }}</span></h3>
-    <div v-if="hayOrganizacion">
-        <h4><span class="badge badge-success"><i class="fas fa-university"></i> {{ organizacion }}</span></h4>
-    </div>
-    <div v-else>
-        <form action="../php/ingresarEntidad.php" method="POST">
-            {{ organizacion }}
-        No perteneces a ninguna organización.<br>
-        <input type="text" placeholder="Código de entidad" required="true" name="codigo">
-        <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-sign-in-alt"></i> Unirme ahora</button>
-        </form>
-    </div>
-    <hr class="my-4">
-    <small class="text-muted">Ver mis datos personales</small><br>
-    <label class="switch">
-        <input type="checkbox"
-        v-model="conCorreo">
-        <span class="slider round"></span>
-    </label>
-    <div v-if="isChecked">
-        <div class="alert alert-secondary" role="alert">
-            <h4>{{ nombre }} {{ apellido }}</h4>
-            <p v-if="hayOrganizacion"><i class="fas fa-university"></i> Organización: {{ organizacion }}</p>
-            <p v-else><i class="fas fa-university"></i> Organización: Ninguna</p>
-            
-            <small>{{ tipoUsuario }}</small>
-            <p><strong>Nombre de usuario: </strong> {{ usuario }}<br>
-            <strong>Edad: </strong> {{ edad }}<br>
-            <strong>Correo electrónico: </strong> {{ correo }}<br>
-            <strong>Teléfono: </strong> {{ telefono }}</p>
-        </div> 
-    </div>
-    
-    <hr class="my-4">
-    <p class="lead">Bienvenido! Esta es tu plataforma de inicio en <strong>iFootprint</strong></p>
-    <div class="alert alert-primary" role="alert">
-        <center>
-    <i class="fas fa-exclamation-triangle"></i> Ahora podrás realizar <strong>calculos de tu huella de carbono</strong>, y estos quedarán guardados en tu <strong>historial</strong>.
-    </center>    
+<div width="300" height="300">
+
 </div>
 
-
-    <div class="alert alert-success" role="alert" v-if="haHechoExamen">
-        <center>
-        <i class="fas fa-question-circle"></i> ¿Quieres realizar otro examen? <br> <br> 
-        <button type="button" class="btn btn-success" onclick="location.href='calculoHuella.php'"><i class="fas fa-feather-alt"></i>  Realizar otro examen</button>
-        </center>
-    </div>
-    <div class="alert alert-success" role="alert" v-else>
-        <center>
-        <i class="fas fa-question-circle"></i> ¡Vaya! <strong>Detectamos que aún no has realizado tu primer examen<br><br>
-        <button type="button" class="btn btn-success" onclick="location.href='calculoHuella.php'"><i class="fas fa-feather-alt"></i> Realizar mi primer examen</button>
-        </center>
-    </div>
-
-    <div class="alert alert-danger" role="alert" v-if="haHechoExamen"><center>
-        <p><strong><i class="fas fa-biohazard"></i> Cantidad de examenes hechos:</strong>
-        <h4> {{ cantidad }} </h4>
-    </div>
-
-    <div class="alert alert-warning" role="alert" v-if="haHechoExamen"><center>
-        <p><strong><i class="fas fa-temperature-low"></i> Promedio de huela de carbono:</strong>
-        <h4> {{ promedio }} toneladas de CO2</h4>
-    </div>
 </div>
-    </div>
-    <div class="col-8">
-        <div class="informes jumbotron">
+
+<script>
+
+</script>
+
+<div id="appVue" class="padreHome container-fluid">
+    <div class="row">
+        <div class="col-4 jumbotron home">
+            <h1>{{ nombre }} <span class="badge badge-secondary"><i class="fas fa-users"></i> Usuario nuevo</span></h1>
+            <h3 v-show="haHechoExamen">Promedio de huella de carbono: <span class="badge badge-warning"><i class="fas fa-biohazard"></i> {{ promedio }}</span></h3>
+            <div v-if="hayOrganizacion">
+                <h4><span class="badge badge-success"><i class="fas fa-university"></i> {{ organizacion }}</span></h4>
+            </div>
+            <div v-else>
+            <form action="../php/ingresarEntidad.php" method="POST">
+                {{ organizacion }}
+            No perteneces a ninguna organización.<br>
+            <input type="text" placeholder="Código de entidad" required="true" name="codigo">
+            <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-sign-in-alt"></i> Unirme ahora</button>
+            </form>
+            </div>
+            <hr class="my-4">
+            <small class="text-muted">Ver mis datos personales</small><br>
+            <label class="switch">
+            <input type="checkbox"
+            v-model="conCorreo">
+            <span class="slider round"></span>
+            </label>
+            <div v-if="isChecked">
+                <div class="alert alert-secondary" role="alert">
+                    <h4>{{ nombre }} {{ apellido }}</h4>
+                    <p v-if="hayOrganizacion"><i class="fas fa-university"></i> Organización: {{ organizacion }}</p>
+                    <p v-else><i class="fas fa-university"></i> Organización: Ninguna</p>
+                    
+                    <small>{{ tipoUsuario }}</small>
+                    <p><strong>Nombre de usuario: </strong> {{ usuario }}<br>
+                    <strong>Edad: </strong> {{ edad }}<br>
+                    <strong>Correo electrónico: </strong> {{ correo }}<br>
+                    <strong>Teléfono: </strong> {{ telefono }}</p>
+                </div> 
+            </div>
+            <hr class="my-4">
+            <p class="lead">Bienvenido! Esta es tu plataforma de inicio en <strong>iFootprint</strong></p>
+            <div class="alert alert-primary" role="alert">
+            <center>
+            <i class="fas fa-exclamation-triangle"></i> Ahora podrás realizar <strong>calculos de tu huella de carbono</strong>, y estos quedarán guardados en tu <strong>historial</strong>.
+            </center>    
+            </div>
+            <div class="alert alert-success" role="alert" v-if="haHechoExamen">
+                <center>
+                <i class="fas fa-question-circle"></i> ¿Quieres realizar otro examen? <br> <br> 
+                <button type="button" class="btn btn-success" onclick="location.href='calculoHuella.php'"><i class="fas fa-feather-alt"></i>  Realizar otro examen</button>
+                </center>
+            </div>
+            <div class="alert alert-success" role="alert" v-else>
+                <center>
+                <i class="fas fa-question-circle"></i> ¡Vaya! <strong>Detectamos que aún no has realizado tu primer examen<br><br>
+                <button type="button" class="btn btn-success" onclick="location.href='calculoHuella.php'"><i class="fas fa-feather-alt"></i> Realizar mi primer examen</button>
+                </center>
+            </div>
+            <div class="alert alert-danger" role="alert" v-if="haHechoExamen"><center>
+                <p><strong><i class="fas fa-biohazard"></i> Cantidad de examenes hechos:</strong>
+                <h4> {{ cantidad }} </h4>
+            </div>
+            <div class="alert alert-warning" role="alert" v-if="haHechoExamen"><center>
+                <p><strong><i class="fas fa-temperature-low"></i> Promedio de huela de carbono:</strong>
+                <h4> {{ promedio }} toneladas de CO2</h4>
+            </div>
+        </div>
+        <div class="col-7 jumbotron informe" v-show="haHechoExamen">
             <h1>Historial de calculos</h1>
             <table class="table table-striped">
             <thead class="thead-dark">
@@ -154,55 +156,14 @@
                 </tr>                
             </tbody>
             </table>
-            <canvas id="myChart" width="400" height="400"></canvas>
+            <canvas id="myChart" width="500" height="500"></canvas>
         </div>
     </div>
 </div>
 
 
-
-
 </div>
 
-<script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-</script>
 
 
         <!-- Script -->
@@ -211,12 +172,16 @@ var myChart = new Chart(ctx, {
             el:'#appVue',
             data:{
                 conCorreo: false,
-                examenes: []             
+                examenes: [],
+                labels: [],             
             },
             mounted () {
                 axios
                 .get('obtenerCalculos.php')
-                .then(response => (this.examenes = response.data))
+                .then(response => (
+                    this.examenes = response.data;
+                    this.labels.push('Naranja'); 
+                    ))
             },
             computed: {
                 isChecked() {
@@ -263,6 +228,44 @@ var myChart = new Chart(ctx, {
                 
             }
             })
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
         </script>
 
 </body>
